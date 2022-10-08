e412f1e50d9582af058bb6473f8210552d69d626
phamquoctai <phamquoctai09082001@gmail.com>
2022-10-08 09:39:54 +0700

    fix bug NO Access Control... Header

--- src/server.js
+++ src/server.js
@@ -9,23 +9,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 // Add headers before the routes are defined
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://api-rental-carl.herokuapp.com');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
 });
 app.use(cors());
 app.use(morgan("combined"));
