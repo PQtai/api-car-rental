b45fb69750040dc18512c3d5969ad92ce76f7054
phamquoctai <phamquoctai09082001@gmail.com>
2022-10-20 14:41:46 +0700

    config token show rentedcars

--- src/api/routes/rentedCarInfo.router.js
+++ src/api/routes/rentedCarInfo.router.js
@@ -3,7 +3,7 @@ import rentedCarInfoControllers from '../controllers/rentedCarInfo.controller.js
 import authMiddleware from '../middlewares/auth.middleware.js';
 const router = express.Router();
 router.post('/create', authMiddleware.verifyToken,rentedCarInfoControllers.create);
-router.get('/:id', authMiddleware.authIsAdmin,rentedCarInfoControllers.show);
+router.get('/:id', authMiddleware.verifyToken,rentedCarInfoControllers.show);
 router.patch('/:id/edit', authMiddleware.authIsAdmin,rentedCarInfoControllers.update);
 router.get('/', authMiddleware.authIsAdmin,rentedCarInfoControllers.index);
 export default router;
